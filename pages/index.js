6903983155031cd86957d243989d823226a75a5d
yucheng <yuchenghsu@gmail.com>
2022-09-09 17:42:43 +0800

    get the raw functionality together

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { blogPosts } from '../lib/postData'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -14,6 +16,19 @@ export default function Home() {
           MY BLOG
         </h1>
       </main>
+      <div>
+        {blogPosts.map((post) => (
+          <div key={post.slug}>
+            <div>
+              <Link href={`/blog/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </div>
+            <div>{post.date.toString()}</div>
+            <div>{post.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
